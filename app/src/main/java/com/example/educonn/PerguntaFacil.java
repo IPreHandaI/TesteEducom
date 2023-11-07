93758edca6a86101a7d580147fbb61f861079b29
IPreHandaI <yanaluiza26y@gmail.com>
2023-11-07 13:39:08 -0300

    Teste Certo

---
+++ app/src/main/java/com/example/educonn/PerguntaFacil.java
@@ -0,0 +1,16 @@
+package com.example.educonn;
+public class PerguntaFacil {
+
+    public static String question[] = {
+            "Quanto e 2 + 2",
+
+    };
+
+    public static String choices[][] = {
+            {"A", "B", "C", "D"}
+    };
+
+    public static String correctAnswer[] = {
+            "A"
+    };
+}
